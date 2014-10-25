50f7ee3d6e98c2160bb6b4b9a9977ed9c17ccd0f
Peter Cai <jnupeter@gmail.com>
2014-10-25 23:04:35 +1000

    add failsafe plugin and integration test class

---
+++ src/test/java/integration/testing/WhatEver.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package integration.testing;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author peter
+ */
+public class WhatEver {
+    
+    @Test
+    public void aTest() {
+        System.out.println("a integration testing not align with naming convention....");
+        assertTrue(true);
+    }
+    
+}
